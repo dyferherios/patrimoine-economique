03a05985f589f3a39fc76fa30763230b8b24a484
dyferherios <hei.freddy.2@gmail.com>
2024-09-06 19:22:05 +0300

    chore: change dependacies

--- template/vite.config.js
+++ template/vite.config.js
@@ -6,15 +6,24 @@
 //   plugins: [react()],
 // })
 // // 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     rollupOptions: {
+//       external: ['axios']  
+//     }
+//   }
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios']  // Add axios as an external dependency
-    }
-  }
+  plugins: [react(), legacy()],
 })
+
