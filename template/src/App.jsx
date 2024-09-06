ac71392dbaea57c0c1381658f3ef3a8772a79783
dyferherios <hei.freddy.2@gmail.com>
2024-09-06 18:10:53 +0300

    fix: importation

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
 import PossessionsContainer from './containers/PossessionsContainer';
